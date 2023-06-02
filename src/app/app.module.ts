596640d49d7a56f87be24fb38d84d21e5d469ad2
DuongLuc2003 <106455957+DuongLuc2003@users.noreply.github.com>
2023-06-02 22:21:20 +0700

    base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
+import { DetailProductPageComponent } from './pages/detail-product-page/detail-product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    NotfoundPageComponent,
+    DetailProductPageComponent
   ],
   imports: [
     BrowserModule,
